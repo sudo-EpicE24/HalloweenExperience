3a5d2ade9acbd73e1ac7b41f509ed66975b685cd
Ryan <ryan.b.grube@gmail.com>
2024-12-10 10:41:38 -0700

    updated houses

---
+++ House.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class House{
+
+    public static String[] candyList = {"Twix","Skittles","Snickers","Almond Joy"};
+
+    private String isDecorated;
+    private ArrayList<String> candyTypes = new ArrayList<String>();
+
+    public House(){
+
+        //select NEW random candy
+        String candyHolder = candyList[];
+        while(!candyTypes.contains(candyHolder)){
+
+        }
+
+        candyTypes.add()
+    }
+
+    public ArrayList<String> getCandy(int amt){
+
+    }
+
+}
