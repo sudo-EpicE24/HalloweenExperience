e6d732db035844935804bc0829c4cb865ccfd143
Ryan <ryan.b.grube@gmail.com>
2024-12-10 10:46:20 -0700

    update House

--- House.java
+++ House.java
@@ -5,14 +5,20 @@ public class House{
     public static String[] candyList = {"Twix","Skittles","Snickers","Almond Joy"};
 
     private String isDecorated;
-    private ArrayList<String> candyTypes = new ArrayList<String>();
+    private ArrayList<String> possibleCandyTypes = new ArrayList<String>();
 
     public House(){
 
         //select NEW random candy
         String candyHolder = candyList[];
-        while(!candyTypes.contains(candyHolder)){
+        int amtCandiesSelected = 0;
 
+        //amt of candy to be selected for this house
+        int totalCandies = Math.random() * candyList.size();
+        
+        while(!candyTypes.contains(candyHolder) || !(amtCandiesSelected < )){
+            candyTypes.add(candyHolder);
+            candyHolder
         }
 
         candyTypes.add()
