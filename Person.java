f6c5a710cd9a6854d1ce1ddcd8e2fc0660206fd5
sudo-EpicE24 <evandisepic@gmail.com>
2024-12-10 11:00:23 -0700

    added personInfo

--- Person.java
+++ Person.java
@@ -6,12 +6,27 @@ public class Person {
 
     private String name;
     private String costume;
-    private Integer[house.candyList] candyCount;
+    private int age;
+    private int candyCount[] = new int[House.candyList.length];
 
     public Person() {
         name = "John Doe";
         costume = "Regular cloths";
-        
+        age = 0;
+        for (int i = 0; i < candyCount.length; i++) {
+            candyCount[i] = 0;
+        }
     }
+
+    public void personInfo() {
+        System.out.println(name+" is a "+age+" year old wearing "+costume);
+        System.out.print("They have ");
+        for (int i = 0; i < candyCount.length; i++) {
+            if (candyCount[i] > 0) {
+                System.out.println(candyCount[i]+" "+House.candyList[i]+", ");
+            }
+        }
+
+    } 
 
 }
