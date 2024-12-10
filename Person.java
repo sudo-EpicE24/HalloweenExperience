84f78bdb247a6489e22c9d8f75149385860eb1ff
sudo-EpicE24 <evandisepic@gmail.com>
2024-12-10 10:42:27 -0700

    new person stuff

--- Person.java
+++ Person.java
@@ -1,11 +1,17 @@
-import java.util.ArrayList;
 
 public class Person {
-    private static ArrayList<String> tricks;
+    private static String[] tricks = {
+        "TP house", "Place raw egg on porch", "Ring doorbell and leave"
+    };
+
     private String name;
-    
+    private String costume;
+    private Integer[house.candyList] candyCount;
 
     public Person() {
-
+        name = "John Doe";
+        costume = "Regular cloths";
+        
     }
+
 }
