ae204ff8493cfcd4ef75a0037a9b8a5a5d918d1d
sudo-EpicE24 <evandisepic@gmail.com>
2024-12-11 10:09:42 -0700

    started candy

--- Candy.java
+++ Candy.java
@@ -1,7 +1,41 @@
-public class Candy {
-    int name;
-    
-    public Candy(){
 
+import java.util.ArrayList;
+
+public class Candy {
+    private String name;
+    private int healValue;
+    // private int rarity; if wanted
+    // private int size; if wanted
+    private static ArrayList<Candy> candies;
+    
+    public Candy(String name){
+        this(name, 0);
+    }
+    public Candy(String name, int healValue) {
+        this.name = name;
+        this.healValue = healValue;
+
+        candies.add(this);
+    }
+
+    public String candyName() {
+        return name;
+    }
+    public int candyHP() {
+        return healValue;
+    }
+    
+    public static String candyName(int i) {
+        return candies.get(i).candyName();
+    }
+    public static int candyHP(int i) {
+        return candies.get(i).candyHP();
+    }
+
+    public static Candy returnCandy(int i) {
+        return candies.get(i);
+    }
+    public static int candyTypes() {
+        return candies.size();
     }
 }
