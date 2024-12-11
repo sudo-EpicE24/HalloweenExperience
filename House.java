c3d7e7aedb9379ef4e5998961b53116ac1df72fc
Ryan <ryan.b.grube@gmail.com>
2024-12-11 10:31:59 -0700

    Updated house

--- House.java
+++ House.java
@@ -1,11 +1,14 @@
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Dictionary;
+import java.util.Hashtable;
 
 public class House{
 
+    public static int maxCandy = 10;
+
     private boolean isDecorated;
-    private ArrayList<Integer> candyInventory;
+    private ArrayList<Candy> candyInventory;
     private double investmentLevel;
     private double generosity;
 
@@ -29,8 +32,30 @@ public class House{
         this.isDecorated = (investmentLevel  > 0.6) ? true : false;
     }
 
-    public Dictionary<Integer, Candy> getCandy(int amt){
-        list = new Dictionary<Integer, Candy>();
+    //returns true for treat, false for trick
+    public boolean trickOrTreat(){
+        return (generosity  > 0.3) ? true : false;
+    }
+
+    public void getTricked(){
+        generosity += 0.05;
+    }
+    //Returns a sum of candy as <Candy, amount>
+    public Dictionary<Candy, Integer> getCandy(){
+
+        Dictionary<Candy, Integer> list = new Hashtable<>();
+
+        int totalCandy = (int) (this.generosity * maxCandy);
+        int counter = 0;
+
+        for(int i=0; i<candyInventory.size(); i++){
+            int amt = (int) Math.min(Math.random() * ((totalCandy * 1.0 )/3) , 1.0 * (totalCandy - counter));
+            list.put(candyInventory.get(i), amt);
+            counter += amt;
+        };
+
+        generosity -= 0.05;
+        return list;
     }
 
 }
