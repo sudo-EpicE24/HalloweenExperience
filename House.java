c66d99e103bb4810b031eb026770fc2f0cbbc15e
Ryan <ryan.b.grube@gmail.com>
2024-12-11 11:01:39 -0700

    Updates house to use maps

--- House.java
+++ House.java
@@ -1,19 +1,22 @@
 import java.util.ArrayList;
 import java.util.Dictionary;
+import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.Map;
 
 public class House{
 
-    public static int maxCandy = 10;
+    public static int maxCandyPerType = 50;
+    public static int maxCandyToGive = 15;
 
     private boolean isDecorated;
-    private ArrayList<Candy> candyInventory;
+    private Map<Candy,Integer> candyInventory;
     private double investmentLevel;
     private double generosity;
+    private String address;
 
     public House(){
-
-        this.candyInventory = new ArrayList<Candy>();
+        this.candyInventory = new Hashtable<>();
         this.investmentLevel = Math.random();
         this.generosity = Math.random();
 
@@ -22,13 +25,22 @@ public class House{
         //select (amtOfCandyTypes) amount of candies from candyList and add their index in candyList to (candyIndexs)
         for(int i = 0; i < amtOfCandyTypes ; i ++){
             int randIndex = (int) Math.random() * Candy.candyTypes();
-            while(candyInventory.contains(Candy.returnCandy(randIndex))){
+            while(candyInventory.get(Candy.returnCandy(randIndex)) != null){
                 randIndex = (int) Math.random() * Candy.candyTypes();
             }
-            this.candyInventory.add(Candy.returnCandy(randIndex));
+            this.candyInventory.put(Candy.returnCandy(randIndex), (int) (investmentLevel * maxCandyPerType));
         }
 
         this.isDecorated = (investmentLevel  > 0.6) ? true : false;
+        this.address = "123 Main St.";
+    }
+    public House(String address){
+        this();
+        this.address = address;
+    }
+
+    public String getAddress(){
+        return this.address;
     }
 
     //returns true for treat, false for trick
@@ -42,17 +54,21 @@ public class House{
     }
     
     //Returns a sum of candy as <Candy, amount>
-    public Dictionary<Candy, Integer> getCandy(){
+    public Map<Candy, Integer> getCandy(){
 
-        Dictionary<Candy, Integer> list = new Hashtable<>();
+        Map<Candy, Integer> list = new HashMap<Candy, Integer>();
 
-        int totalCandy = (int) (this.generosity * maxCandy);
+        int totalCandy = (int) (generosity * maxCandyToGive);
         int counter = 0;
 
-        for(int i=0; i<candyInventory.size(); i++){
-            int amt = (int) Math.min(Math.random() * ((totalCandy * 1.0 )/3) , 1.0 * (totalCandy - counter));
-            list.put(candyInventory.get(i), amt);
+        for(Candy key : candyInventory.keySet()){
+            //amt of candy this house has of this type
+            int allowedCandy = candyInventory.get(key);
+
+            int amt = (int) Math.min(Math.random() * allowedCandy / 10, totalCandy - counter);
+            list.put(key, amt);
             counter += amt;
+            candyInventory.replace(key, candyInventory.get(key) - amt);
         };
 
         this.generosity -= 0.05;
